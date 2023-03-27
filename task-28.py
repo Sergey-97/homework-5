88e4b23c2062779b183266777bfb05da60229fe1
Sergey <aseev-1977@list.ru>
2023-03-27 22:55:34 +0300

    task-28

--- task-28.py
+++ task-28.py
@@ -9,9 +9,22 @@ def sum(a, b):
     elif a == 0:
         return b
     elif a > b:
-        return sum(a-1, b+1)
-    else:
         return sum(a+1, b-1)
+    else:
+        return sum(a-1, b+1)
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 print("Сумма чисел равна:", sum(a, b))
+
+# def sum(a, b):
+#     if b == 0:
+#         return a
+#     elif a == 0:
+#         return b
+#     elif a > b:
+#         return sum(a-b, b+a)
+#     else:
+#         return sum(a+b, b-a)
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# print("Сумма чисел равна:", sum(a, b))
