1007451f3d85ef373e4ca351536f4d77aea84c6b
Sergey <aseev-1977@list.ru>
2023-03-27 22:23:43 +0300

    task-26

--- task-26.py
+++ task-26.py
@@ -2,13 +2,6 @@
 два числа A и B, и возводит число А в целую степень B с
 помощью рекурсии.   """
 
-# def numberA(a):
-#     for i in range(2,int(n**0.5)+1):
-#         if n%i==0:
-#             return "нет"
-#     return "да"
-# print(numberA(int(input("число A = "))))
-
 def power(a, b):
     if b == 0:
         return 1
